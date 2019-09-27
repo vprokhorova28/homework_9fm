e159f0bf10e1e4ca27e4b5e4e174ae8c2505c316
vprokhorova28 <55251241+vprokhorova28@users.noreply.github.com>
2019-09-27 11:58:05 +0300

    Update test_mymatrix.py

--- test_mymatrix.py
+++ test_mymatrix.py
@@ -15,3 +15,17 @@ def test___sub__():
     assert(matrix.get_data() == [[1, 2], [5, 6], [4, 7]])
     assert(matrix1.get_data() == [[1, 2], [5, 6], [4, 1]])
     assert(matrixsum.get_data() == [[0, 0], [0, 0], [0, 6]])
+
+def test___iadd__():
+    matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
+    matrix1 = MyMatrix([[1, 2], [5, 6], [4, 1]])
+    matrix += matrix1
+    assert(matrix.get_data() == [[2, 4], [10, 12], [8, 8]])
+    assert(matrix1.get_data() == [[1, 2], [5, 6], [4, 1]])
+
+def test___isub__():
+    matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
+    matrix1 = MyMatrix([[1, 2], [5, 6], [4, 1]])
+    matrix -= matrix1
+    assert(matrix.get_data() == [[0, 0], [0, 0], [0, 6]])
+    assert(matrix1.get_data() == [[1, 2], [5, 6], [4, 1]])
