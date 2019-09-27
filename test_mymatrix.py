69c174ef5e8affc9e3b775f861f3194c91bf261d
vprokhorova28 <55251241+vprokhorova28@users.noreply.github.com>
2019-09-27 11:51:52 +0300

    Create test_mymatrix.py

---
+++ test_mymatrix.py
@@ -0,0 +1,17 @@
+from mymatrix import MyMatrix
+
+def test___add__():
+    matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
+    matrix1 = MyMatrix([[1, 2], [5, 6], [4, 1]])
+    matrixsum = matrix + matrix1
+    assert(matrix.get_data() == [[1, 2], [5, 6], [4, 7]])
+    assert(matrix1.get_data() == [[1, 2], [5, 6], [4, 1]])
+    assert(matrixsum.get_data() == [[2, 4], [10, 12], [8, 8]])
+
+def test___sub__():
+    matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
+    matrix1 = MyMatrix([[1, 2], [5, 6], [4, 1]])
+    matrixsum = matrix - matrix1
+    assert(matrix.get_data() == [[1, 2], [5, 6], [4, 7]])
+    assert(matrix1.get_data() == [[1, 2], [5, 6], [4, 1]])
+    assert(matrixsum.get_data() == [[0, 0], [0, 0], [0, 6]])
