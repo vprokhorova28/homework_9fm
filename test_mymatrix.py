2e7d07464e2be33ba3d815b9da029ee73acd9619
vprokhorova28 <55251241+vprokhorova28@users.noreply.github.com>
2019-10-04 12:01:16 +0300

    Update test_mymatrix.py

--- test_mymatrix.py
+++ test_mymatrix.py
@@ -32,27 +32,27 @@ def test___isub__():
     assert(matrix1.get_data() == [[1, 2], [5, 6], [4, 1]])
 
 def test_size():
-    zero_matrix = []
+    zero_matrix = MyMatrix([])
     size = zero_matrix.size()
     matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
     s = matrix.size()
-    assert(size == (0, 0))
-    assert(s == (3, 2))
+    assert(zero_matrix.size() == (0, 0))
+    assert(matrix.size() == (3, 2))
 
 def test_flip_up_down():
-    zero_matrix = []
+    zero_matrix = MyMatrix([])
     matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
     assert(matrix.flip_up_down().get_data() == [[4, 7], [5, 6], [1, 2]])
-    assert(zero_matrix.flip_up_down().get_data() = [])
+    assert(zero_matrix.flip_up_down().get_data() == [])
 
 def test_flip_left_right():
-    zero_matrix = []
+    zero_matrix = MyMatrix([])
     matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
     assert(matrix.flip_left_right().get_data() == [[2, 1], [6, 5], [7, 4]])
-    assert(zero_matrix.flip_left_right.get_data() = [])
+    assert(zero_matrix.flip_left_right().get_data() == [])
     
 def test_flipped_up_down():
-    zero_matrix = []
+    zero_matrix = MyMatrix([])
     zero_matrixcopy = zero_matrix.flipped_up_down()
     matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
     matrix2 = matrix.flipped_up_down()
@@ -62,7 +62,7 @@ def test_flipped_up_down():
     assert(zero_matrixcopy.get_data() == [])
 
 def test_flipped_left_right():
-    zero_matrix = []
+    zero_matrix = MyMatrix([])
     zero_matrixcopy = zero_matrix.flipped_left_right()
     matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
     matrix2 = matrix.flipped_left_right()
@@ -72,16 +72,16 @@ def test_flipped_left_right():
     assert(zero_matrixcopy.get_data() == [])
 
 def test_transpose():
-    zero_matrix = []
+    zero_matrix = MyMatrix([])
     matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
     matrix.transpose()
     assert(matrix.size() == (2, 3))
     assert(matrix.get_data() == [[1, 5, 4], [2, 6, 7]])
-    assert(zero_matrix.transpose.get_data() == [])
+    assert(zero_matrix.transpose().get_data() == [])
 
 
-def def test_transposed():
-    zero_matrix = []
+def test_transposed():
+    zero_matrix = MyMatrix([])
     zero_matrixcopy = zero_matrix.transposed()
     matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
     matrix2 = matrix.transposed()
@@ -89,3 +89,16 @@ def def test_transposed():
     assert(matrix2.get_data() == [[1, 5, 4], [2, 6, 7]])
     assert(zero_matrix.get_data() == [])
     assert(zero_matrixcopy.get_data() == [])
+
+def test___setitem__():
+    matrix = MyMatrix([[1, 2], [5, 100], [4, 7]])
+    matrix[0][1] = 6
+    # matrix[1, 1] = 100
+    assert(matrix.get_data() == [[1, 6], [5, 100], [4, 7]])
+    raise Exception
+
+def test___getitem__():
+    matrix = MyMatrix([[1, 2], [5, 6], [4, 7]])
+    assert(matrix[0][1] == 2)
+    # assert(matrix[1, 1] == 6)
+    raise Exception
